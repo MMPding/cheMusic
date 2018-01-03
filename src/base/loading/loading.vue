9bc951687248df73b0dd9a5b9e4e0a903e684621
MMPding <qldsdsg@aliyun.com>
2018-01-03 09:21:37 +0800

    推荐页面开发完成

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loading">
+    <img src="./loading.gif" width="24" height="24">
+    <p class="desc">{{title}}</p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: 'Loading...'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable.styl"
+  .loading
+    width: 100%
+    text-align: center
+    .desc
+      line-height: 20px
+      font-size: $color-text-l
+</style>
